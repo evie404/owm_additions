d1dd9b2b9a72e7114131aa8b4a5ba179aa6e01a6
Evelyn Pai <wowitsevelynwow+github@gmail.com>
2022-09-04 17:55:57 -0700

    mercy royal knight

--- heroes/mercy/all.py
+++ heroes/mercy/all.py
@@ -4,6 +4,7 @@ from ...models.bone_group_mapping import BoneGroupMapping
 from .atlantic import ATLANTIC_MAPPING
 from .default import DEFAULT_MAPPING
 from .dr_ziegler import DR_ZIEGLER_MAPPING
+from .royal_knight import ROYAL_KNIGHT_MAPPING
 from .seolbim import SEOLBIM_MAPPING
 from .winged_victory import WINGED_VICTORY_MAPPING
 from .witch import WITCH_MAPPING
@@ -16,5 +17,6 @@ MERCY_MAPPING: Dict[str, Dict[str, Dict[str, BoneGroupMapping]]] = {
     | WINGED_VICTORY_MAPPING
     | WITCH_MAPPING
     | SEOLBIM_MAPPING
-    | ZHUQUE_MAPPING,
+    | ZHUQUE_MAPPING
+    | ROYAL_KNIGHT_MAPPING,
 }
