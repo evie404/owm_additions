7e01e400ca31729f9074078f2978b2fd148ef9dd
Evelyn Pai <wowitsevelynwow+github@gmail.com>
2022-09-04 17:56:01 -0700

    import into collection

--- op_import_skin.py
+++ op_import_skin.py
@@ -1,7 +1,7 @@
-from typing import List, Set
+from typing import List, Optional, Set
 
 import bpy
-from bpy.types import Context
+from bpy.types import Context, LayerCollection
 
 from owm_additions.hero_skins_prop import get_context_hero_name, get_context_skin_name
 from owm_additions.paths.skin import entity_paths
@@ -17,11 +17,42 @@ class OWM_ADD_ImportSkin(bpy.types.Operator):
         hero = get_context_hero_name(context)
         skin = get_context_skin_name(context)
 
+        collection_name = f"{hero} ({skin})"
+
+        if (
+            len(context.collection.children) > 0
+            or len(context.collection.all_objects) > 0
+            or context.collection.name == "Scene Collection"
+        ):
+            collection = bpy.data.collections.new(collection_name)
+            context.collection.children.link(collection)
+
+            layer_collection = find_layer_collection(
+                context.view_layer.layer_collection, collection.name
+            )
+            context.view_layer.active_layer_collection = layer_collection
+        else:
+            context.collection.name = collection_name
+
         import_skin(hero, skin)
 
         self.report({"INFO"}, f"Finished import {hero} ({skin}).")
 
         return {"FINISHED"}
+
+
+def find_layer_collection(
+    root: LayerCollection, collection_name: str
+) -> Optional[LayerCollection]:
+    found: Optional[LayerCollection] = None
+
+    if root.name == collection_name:
+        return root
+
+    for layer in root.children:
+        found = find_layer_collection(layer, collection_name)
+        if found:
+            return found
 
 
 def import_skin(hero: str, skin: str) -> None:
