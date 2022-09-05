a80dedb946f2339617de1e4469c48fd135af177b
Evelyn Pai <wowitsevelynwow+github@gmail.com>
2022-09-04 20:47:19 -0700

    organize imports and stagger

--- dev/op_dev_import_all_skins.py
+++ dev/op_dev_import_all_skins.py
@@ -1,9 +1,10 @@
 import glob
+import math
 import os
 from typing import List, Set
 
 import bpy
-from bpy.types import Context
+from bpy.types import Context, Object
 
 from ..importing.asset_prop import get_context_hero_name
 from ..organize.collection import (
@@ -11,6 +12,7 @@ from ..organize.collection import (
     new_child_collection,
     rename_or_new_child_collection,
 )
+from ..organize.op_organize_hero_objs import find_root, ow_organize_character_objects
 from ..paths.skin import entity_paths, list_all_skins_of_hero
 
 
@@ -39,6 +41,8 @@ def import_all_skins(context: Context, hero: str) -> None:
 
     orig_layer_collection = context.view_layer.active_layer_collection
 
+    root_objs: List[Object] = []
+
     for skin in skins:
         skin_paths: List[str] = entity_paths(hero, skin)
         skin_path = skin_paths[0]
@@ -54,3 +58,24 @@ def import_all_skins(context: Context, hero: str) -> None:
             # importMaterial=False,
             import_children=False,
         )
+
+        root_obj = find_root(context)
+
+        ow_organize_character_objects(
+            collection=context.collection,
+            root_object=root_obj,
+            hero=hero,
+            skin=skin,
+        )
+
+        root_objs.append(root_obj)
+
+    stagger_objects(root_objs)
+
+
+def stagger_objects(arm_objs: List[Object], spacing: int = 2) -> None:
+    divisor = int(math.ceil(math.sqrt(len(arm_objs))))
+
+    for i, obj in enumerate(arm_objs):
+        obj.location.x = (i % divisor) * spacing  # - divisor * spacing
+        obj.location.y = (i // divisor) * spacing  # - divisor * spacing
