0ac26a944e4636b7489454c5b3d93e29168bffda
Evelyn Pai <wowitsevelynwow+github@gmail.com>
2022-09-04 17:55:59 -0700

    dynamic skins

--- paths.py
+++ paths.py
@@ -1,6 +1,62 @@
 import glob
 import os
-from typing import List
+from typing import Dict, List
+
+BASE_PATH = os.path.join(
+    "D:",
+    os.sep,
+    "Game Extracts",
+    "Overwatch",
+)
+
+
+def list_all_heroes(base_path: str = BASE_PATH) -> List[str]:
+    search_pattern = os.path.join(base_path, "Heroes", "*")
+
+    return [path.split(os.sep)[-1] for path in glob.glob(search_pattern)]
+
+
+def list_all_skins_of_hero(hero: str, base_path: str = BASE_PATH) -> List[str]:
+    skins: List[str] = []
+
+    prefix = os.path.join(base_path, "Heroes", hero, "Skin") + os.sep
+    suffix = os.sep + os.path.join(
+        "Entities",
+        "HeroGallery",
+        "HeroGallery.owentity",
+    )
+
+    no_rarity_paths: List[str] = glob.glob(f"{prefix}*{os.sep}*{suffix}")
+
+    skins = [
+        skin.replace(prefix, "").replace(suffix, "").split(os.sep)[-1]
+        for skin in no_rarity_paths
+    ]
+
+    rarity_paths: List[str] = glob.glob(f"{prefix}*{os.sep}*{os.sep}*{suffix}")
+
+    skins += [
+        skin.replace(prefix, "").replace(suffix, "").split(os.sep)[-1]
+        for skin in rarity_paths
+    ]
+
+    skins.sort()
+
+    return skins
+
+
+def list_all_skins(base_path: str = BASE_PATH) -> Dict[str, List[str]]:
+    all_skins: Dict[str, List[str]] = {}
+
+    heroes = list_all_heroes(base_path)
+    heroes.sort()
+
+    for hero in heroes:
+        skins = list_all_skins_of_hero(hero, base_path)
+
+        all_skins[hero] = skins
+
+    return all_skins
 
 
 def unsanitize_name(name: str) -> str:
@@ -20,16 +76,17 @@ def entity_paths(hero: str, skin: str) -> List[str]:
     return entity_paths
 
 
-def skin_search_paths(hero: str, skin: str) -> List[str]:
+def skin_search_paths(
+    hero: str,
+    skin: str,
+    base_path: str = BASE_PATH,
+) -> List[str]:
     hero_path = sanitize_name(hero)
     skin_path = sanitize_name(skin)
 
     return [
         os.path.join(
-            "D:",
-            os.sep,
-            "Game Extracts",
-            "Overwatch",
+            base_path,
             "Heroes",
             hero_path,
             "Skin",
@@ -40,10 +97,7 @@ def skin_search_paths(hero: str, skin: str) -> List[str]:
             "HeroGallery.owentity",
         ),
         os.path.join(
-            "D:",
-            os.sep,
-            "Game Extracts",
-            "Overwatch",
+            base_path,
             "Heroes",
             hero_path,
             "Skin",
